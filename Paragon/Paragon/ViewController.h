fdc0a1249e2ff3071356721c0a5f078bf9c44a73
Richie <rjd232@cornell.edu>
2015-07-15 08:17:43 -0400

    initial project commit, will later move to pod

---
+++ Paragon/Paragon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Paragon
+//
+//  Created by Richie Davis on 7/15/15.
+//  Copyright (c) 2015 Davis Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '7.0'
+
+xcodeproj 'Paragon/Paragon.xcodeproj'
+
+pod "AFNetworking", "~> 2.0
